ff614b124144d1a00d405ab959278fd76374cbb8
Maria <wmasha2006@gmail.com>
2025-03-16 18:23:21 +0300

    изменение

--- дз5_шифр цезаря/дз5_шифр цезаря/Program.cs
+++ дз5_шифр цезаря/дз5_шифр цезаря/Program.cs
@@ -4,65 +4,68 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Сделайте выбор: 1-зашифровать, 2-расшифровать");
-            bool isValid = int.TryParse(Console.ReadLine(), out int choise);
-
-            Console.WriteLine("Введите ключ для шифрования");
-            int n = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите сообщение");
-            string str = Console.ReadLine();
-
-            if (!isValid)
+            int choise = 0;
+            while (true)
             {
-                Console.WriteLine("Не число");
-                return;
-            }
-            else if (choise > 2 || choise < 1)
-            {
-                Console.WriteLine("Другая цифра");
-                return;
-            }
-            else if (choise == 2)
-            {
-                n = -n;
-            }
-
-            string alphabet = "абвгдеёжзиклмнопрстуфхцчшщъыьэюя";
-            string alphabetUpp = alphabet.ToUpper();
-
-            char[] result = str.ToCharArray();
-
-            for (int i = 0; i < result.Length; i++)
-            {
-                bool IsUpper = char.IsUpper(result[i]);
-                bool IsLower = char.IsLower(result[i]);
-
-                if (IsUpper == true)
+                Console.WriteLine("Сделайте выбор: 1-зашифровать, 2-расшифровать");
+                bool isValid = int.TryParse(Console.ReadLine(), out choise);
+                if (!isValid)
                 {
-                    int index = alphabetUpp.IndexOf(result[i]);
-                    int newindex = ((index + n + alphabetUpp.Length) % alphabetUpp.Length);
-                    result[i] = alphabetUpp[newindex];
+                    Console.WriteLine("Не число");
                 }
-                else if (IsLower == true)
+                else if (choise > 2 || choise < 1)
                 {
-                    int index = alphabet.IndexOf(result[i]);
-                    int newindex = ((index + n + alphabetUpp.Length) % alphabetUpp.Length);
-                    result[i] = alphabet[newindex];
+                    Console.WriteLine("Ошибка ввода");
                 }
                 else
-                {
-                    if (!char.IsLetter(result[i]))
-                        continue;
-                }
+                    break;
             }
 
-            Console.WriteLine("Результат: ");
-            for (int i = 0; i < result.Length; i++)
-            {
-                Console.Write( result[i]);
-            }
-            Console.WriteLine();
+                Console.WriteLine("Введите ключ для шифрования");
+                int n = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите сообщение");
+                string str = Console.ReadLine();
+
+                if (choise == 2)
+                {
+                    n = -n;
+                }
+
+                string alphabet = "абвгдеёжзиклмнопрстуфхцчшщъыьэюя";
+                string alphabetUpp = alphabet.ToUpper();
+
+                char[] result = str.ToCharArray();
+
+                for (int i = 0; i < result.Length; i++)
+                {
+                    bool IsUpper = char.IsUpper(result[i]);
+                    bool IsLower = char.IsLower(result[i]);
+
+                    if (IsUpper == true)
+                    {
+                        int index = alphabetUpp.IndexOf(result[i]);
+                        int newindex = ((index + n + alphabetUpp.Length) % alphabetUpp.Length);
+                        result[i] = alphabetUpp[newindex];
+                    }
+                    else if (IsLower == true)
+                    {
+                        int index = alphabet.IndexOf(result[i]);
+                        int newindex = ((index + n + alphabetUpp.Length) % alphabetUpp.Length);
+                        result[i] = alphabet[newindex];
+                    }
+                    else
+                    {
+                        if (!char.IsLetter(result[i]))
+                            continue;
+                    }
+                }
+                Console.WriteLine("Результат: ");
+                for (int i = 0; i < result.Length; i++)
+                {
+                    Console.Write(result[i]);
+                }
+                Console.WriteLine();
         }
     }
 }

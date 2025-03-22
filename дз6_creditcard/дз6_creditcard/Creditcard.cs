bbc4d48360f99a2863e5eb28a082b9f26766b80f
Maria <wmasha2006@gmail.com>
2025-03-22 13:20:11 +0300

    изменение

---
+++ дз6_creditcard/дз6_creditcard/Creditcard.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace дз6_creditcard
+{
+    class Creditcard
+    {
+        public string accountnumber;
+        public double balance;
+
+        public Creditcard(string accountnumber, double balance)
+            {
+            this.accountnumber = accountnumber;
+            this.balance = balance;
+            }
+
+        public void Add()
+        {
+            Console.WriteLine("Введите сумму для зачисления");
+            double ammount = int.Parse(Console.ReadLine());
+            this.balance += ammount;
+            Console.WriteLine($"На счет{this.accountnumber} зачислено {ammount}");
+        }
+
+        public void Withdraw()
+        {
+            Console.WriteLine("Введите сумму для снятия");
+            double ammount = int.Parse(Console.ReadLine());
+
+            if (ammount>0 || ammount<=this.balance)
+            {
+                this.balance -= ammount;
+                Console.WriteLine($"Со счета{this.accountnumber} сняли {ammount}");
+            }
+            else
+                Console.WriteLine("Недосататочно средств");
+        }
+
+        public void Showinfo()
+        {
+            Console.WriteLine($"Номер счета:{this.accountnumber}, баланс:{this.balance}");
+        }
+
+    }
+}
---
+++ дз6_creditcard/дз6_creditcard/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace дз6_creditcard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Creditcard number1 =new Creditcard("1234",1000);
+            Creditcard number2 = new Creditcard("2345", 3000);
+            Creditcard number3 = new Creditcard("3456", 5000);
+
+            while(true)
+            {
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1-Начислить сумму на счёт");
+                Console.WriteLine("2-Снять сумму со счёта");
+                Console.WriteLine("3-Информация о счёте");
+                Console.WriteLine("4-Выход");
+
+                int choice2 = int.Parse(Console.ReadLine());
+                if (choice2 == 4)
+                    return;
+
+                Console.WriteLine("Введите номер счета");
+                Console.WriteLine("1-1234");
+                Console.WriteLine("2-2345");
+                Console.WriteLine("3-3456");
+                int choice1 = int.Parse(Console.ReadLine());
+
+                Creditcard choicenumber = new Creditcard(null, 0);
+
+                switch(choice1)
+                {
+                    case 1: 
+                        choicenumber = number1;
+                        break;
+                    case 2:
+                        choicenumber = number2;
+                        break;
+                    case 3:
+                        choicenumber = number3;
+                        break;
+                    default:
+                        Console.WriteLine("Неверный ввод");
+                        break;
+                }
+
+                switch (choice2)
+                {
+                    case 1: 
+                        choicenumber.Add();
+                        break;
+                    case 2:
+                        choicenumber.Withdraw();
+                        break;
+                    case 3:
+                        choicenumber.Showinfo();
+                        break;
+                    default:
+                        Console.WriteLine("Неверный ввод");
+                        break;
+                }
+            }
+        }
+
+    }
+}
